73e0f0c9610194e3118a4d00398222f44dffc087
Sudeep <sudeep.rp@gmail.com>
2025-07-29 15:59:39 +0530

    complete the test for empty input

--- statistics.test.py
+++ statistics.test.py
@@ -1,5 +1,6 @@
 import unittest
 import statistics
+import math
 
 
 class StatsTest(unittest.TestCase):
@@ -12,10 +13,9 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
-    # All fields of computedStats (average, max, min) must be
-    # nan (not-a-number), as defined in the math package
-    # Specify the assert here.
-    # Use nan and isnan in https://docs.python.org/3/library/math.html
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
 
 
 if __name__ == "__main__":
