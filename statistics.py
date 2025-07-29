49518c515873552d62f302f3203f8538f28e45e0
Sudeep <sudeep.rp@gmail.com>
2025-07-29 15:57:22 +0530

    stats including empty input

--- statistics.py
+++ statistics.py
@@ -1,4 +1,13 @@
 
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
-  return None
+  stats = {}
+  if numbers:
+    stats["avg"] = sum(numbers) / len(numbers)
+    stats["max"] = max(numbers)
+    stats["min"] = min(numbers)
+  else:
+    stats["avg"] = float('nan')
+    stats["max"] = float('nan')
+    stats["min"] = float('nan')
+  return stats
